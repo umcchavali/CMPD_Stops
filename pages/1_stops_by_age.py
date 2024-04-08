d03abfa348ee16eeffe589f603f9a98787f1b721
Micah Richardson <micahronrichardson@gmail.com>
2024-04-08 19:33:29 -0400

    added box plot

--- pages/1_stops_by_age.py
+++ pages/1_stops_by_age.py
@@ -18,3 +18,13 @@ age_bar = alt.Chart(traffic).mark_bar().encode(
 ).properties(width=800,height=400)
 
 st.altair_chart(age_bar)
+
+## Box plot
+chart2 = alt.Chart(traffic).mark_boxplot().encode(
+    x = alt.X('Was_a_Search_Conducted'),
+    y = alt.Y('Driver_Age')
+).properties(
+    width = 500,
+    title = 'Boxplot between Search Conducted vs Driver Age')
+
+st.altair_chart(chart2)
